6ab280ff97f964fd867d13fbd12cb52b12e49920
katherinenjm <145992901+katherinenjm@users.noreply.github.com>
2026-06-06 15:03:15 +0100

    Add: python-heap-pop - submission-0

---
+++ Python Coding Interviews/python-heap-pop/submission-0.py
@@ -0,0 +1,18 @@
+import heapq
+from typing import List
+
+
+def heap_pop(heap: List[int]) -> List[int]:
+    prior_min_list = [0]*len(heap)
+    #print(f"heap = {heap},len(heap)={len(heap)}")
+
+    for i in range(len(heap)):
+        prior_min_list[i] = heapq.heappop(heap)
+        #print(f"prior_min_list at i = {i} is {prior_min_list}")
+    return prior_min_list
+
+
+# do not modify below this line
+print(heap_pop([1, 2, 3]))
+print(heap_pop([1, 3, 2]))
+print(heap_pop([6, 7, 8, 12, 9, 10]))
