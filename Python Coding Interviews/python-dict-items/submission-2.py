41f2b2b639b0f8040cdcaf4215e9f46b1815ab8c
katherinenjm <145992901+katherinenjm@users.noreply.github.com>
2026-06-06 13:47:33 +0100

    Add: python-dict-items - submission-2

---
+++ Python Coding Interviews/python-dict-items/submission-2.py
@@ -0,0 +1,22 @@
+from typing import Dict, List, Tuple
+
+
+def get_dict_items(age_dict: Dict[str, int]) -> List[Tuple[str, int]]:
+    #tuple_out = [0]*len(age_dict.keys())
+    #i=0
+    #for name, age in age_dict.items():
+        #tuple_out[i] = (name, age)
+        #i+= 1
+        #print(tuple_out)
+    #return tuple_out
+    
+    #return [(name, age) for name, age in age_dict.items()]
+
+    return list(age_dict.items())
+
+
+# do not modify below this line
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40}))
+print(get_dict_items({'Bob': 30, 'David': 40, 'Charlie': 35, 'Alice': 25, 'Eve': 45}))
+print(get_dict_items({'Alice': 25, 'Bob': 30, 'Charlie': 35, 'David': 40, 'Eve': 45, 'Frank': 50}))
