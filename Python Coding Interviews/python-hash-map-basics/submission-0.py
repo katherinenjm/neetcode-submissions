3ac8e7450a4a5af509cda9a993ecf245a53d3962
katherinenjm <145992901+katherinenjm@users.noreply.github.com>
2026-06-06 12:28:15 +0100

    Add: python-hash-map-basics - submission-0

---
+++ Python Coding Interviews/python-hash-map-basics/submission-0.py
@@ -0,0 +1,31 @@
+from typing import List, Dict
+
+
+def build_hash_map(keys: List[str], values: List[int]) -> Dict[str, int]:
+    dict = {}
+    for i in range(len(keys)):
+        dict[keys[i]] = values[i]
+
+    return dict
+
+
+def get_values(hash_map: Dict[str, int], keys: List[str]) -> List[int]:
+    #print(len(keys))
+    vals = []
+    for i in range(len(keys)):
+        vals.append(hash_map[keys[i]])
+    #    print(f"key={keys[i]}, value = {hash_map[keys[i]]}, vals = {vals}")
+    return vals
+
+    
+
+
+
+# do not modify below this line
+print(build_hash_map(["Alice", "Bob", "Charlie"], [90, 80, 70]))
+print(build_hash_map(["Jane", "Carol", "Charlie"], [25, 100, 60]))
+print(build_hash_map(["Doug", "Bob", "Tommy"], [80, 90, 100]))
+
+print(get_values({"Alice": 90, "Bob": 80, "Charlie": 70}, ["Alice", "Bob", "Charlie"]))
+print(get_values({"Jane": 25, "Charlie": 60, "Carol": 100, }, ["Jane", "Carol"]))
+print(get_values({"X": 205, "Y": 78, "Z": 100}, ["Y"]))
