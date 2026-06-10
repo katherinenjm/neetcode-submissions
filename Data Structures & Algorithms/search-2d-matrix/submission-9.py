74c51c440ca188b6d881aab0740dd00e7e665463
katherinenjm <145992901+katherinenjm@users.noreply.github.com>
2026-06-10 16:05:38 +0100

    Add: search-2d-matrix - submission-9

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-9.py
@@ -0,0 +1,44 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #do binary search for each row and if reach final rown and no target found return false
+        m = len(matrix)
+        n = len(matrix[0]) -1
+
+        #first we check whihc row the target is in
+
+        i = 0
+        target_row = i
+        while i < m :
+            if target > matrix[i][n]:
+                #target is not in row i, can discard
+                i += 1
+            elif target < matrix[i][n]:
+                target_row = i
+                i += m
+            elif target == matrix[i][n]:
+                return True
+        print(f"target_row = {target_row}")
+                
+            
+
+
+
+        #we now know what row were searching in: matrix[target_row]
+        L = 0
+        R = n
+
+
+
+        #for a given row we implement binary search
+        while L <= R:
+            mid  = (L + R) // 2
+
+            if target < matrix[target_row][mid]:
+                R = mid -1
+            elif target > matrix[target_row][mid]:
+                L = mid + 1
+            else:
+                return True
+        return False
+
+        
